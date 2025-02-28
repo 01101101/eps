c300afdc884e5f6bdd353a159f650d3888d06cee
01101101 <pirat@hey.com>
2025-02-28 11:12:28 +0100

    eps

--- src/components/PropertiesBox.jsx
+++ src/components/PropertiesBox.jsx
@@ -6,11 +6,36 @@ const Separator = () => {
   return <hr className="border-border" />;
 };
 
-const Field = ({ label, value }) => {
+const StringValue = ({ id, property, value }) => {
+  const handleChange = (event) => {
+    useWorkbench.setState((state) => {
+      const widget = state.widgets.find((widget) => widget.id === id);
+      if (property === 'id') {
+        widget.id = event.target.value;
+        state.activeWidgetId = widget.id;
+      } else {
+        if (widget.properties == null) {
+          widget.properties = {};
+        }
+        widget.properties[property].value = event.target.value;
+      }
+    });
+  };
+
   return (
-    <div className="flex flex-col gap-1 p-2">
-      <div className="text-border uppercase">{label}</div>
-      <div>{value}</div>
+    <textarea
+      className="focus:bg-active hover:bg-active empty:bg-active line-clamp-1 field-sizing-content resize-none rounded-sm border border-transparent p-1 outline-none focus:line-clamp-none"
+      value={value}
+      onChange={handleChange}
+    />
+  );
+};
+
+const Field = ({ id, label, type, value }) => {
+  return (
+    <div className="flex flex-col px-2 py-1">
+      <div className="text-border p-1 uppercase">{label}</div>
+      {type === 'string' ? <StringValue id={id} property={label} value={value} /> : null}
     </div>
   );
 };
@@ -19,14 +44,12 @@ export const PropertiesBox = () => {
   const widget = useWorkbench((state) => state.widgets.find(({ id }) => id === state.activeWidgetId));
   const properties = useMemo(() => allWidgets[widget.type].properties, [widget]);
 
-  console.log(properties);
-
   return (
     <div className="border-border absolute -top-px -right-2 flex h-[calc(100%+2px)] w-80 translate-x-full flex-col gap-1 overflow-y-auto rounded-sm border bg-black">
-      <Field label="id" value={widget.id} />
+      <Field id={widget.id} label="id" type="string" value={widget.id} />
       <Separator />
-      {Object.keys(properties).map((key) => (
-        <Field label={key} value={''} />
+      {Object.entries(properties).map(([key, { type }]) => (
+        <Field key={key} id={widget.id} label={key} type={type} value={widget.properties?.[key]?.value} />
       ))}
     </div>
   );
