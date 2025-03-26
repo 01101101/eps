dd668fa70044ade384901de7b767e87be1cc13e7
01101101 <pirat@hey.com>
2025-03-26 22:03:20 +0100

    eps

--- src/app/WidgetPanel/SendAction.jsx
+++ src/app/WidgetPanel/SendAction.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
 import commands from '~/../protocol/data/commands';
 import { Select } from '~/app/Select';
 import { Field } from '~/app/WidgetPanel/Field';
+import { useWorkbench } from '~/app/Workbench';
 
-export const SendAction = ({ action }) => {
-  const [command, setCommand] = useState(null);
+export const SendAction = ({ widget, event, action }) => {
+  const id = widget.id;
 
   const handleChangeCommand = (command) => {
-    setCommand(command);
+    useWorkbench.setState((state) => {
+      const widget = state.widgets.find((widget) => widget.id === id);
+      const stateEvent = widget.events.find(({ id }) => id === event.id);
+      stateEvent.action.properties.command = command;
+    });
   };
 
   return (
     <div className="col-span-2 grid grid-cols-subgrid">
       <Field label="command">
-        <Select value={command} onChange={handleChangeCommand}>
+        <Select value={action.properties.command} onChange={handleChangeCommand}>
           {Object.entries(commands).map(([code, { name }]) => (
             <Select.Option key={name} value={name}>
               {name}
